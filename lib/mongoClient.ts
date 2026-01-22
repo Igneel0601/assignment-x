dddf5819420de17bf2d82015668b1b33a2cbd945
Vaibhav Verma <vermavaibhav241@gmail.com>
2026-01-22 21:41:49 +0530

    final

--- lib/mongoClient.ts
+++ lib/mongoClient.ts
@@ -1,33 +1,37 @@
-import { MongoClient } from "mongodb";
-
+// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+import { MongoClient, ServerApiVersion } from "mongodb"
+ 
 if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
-
-const uri: string = process.env.MONGODB_URI;
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
+ 
+const uri = process.env.MONGODB_URI
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+}
+ 
+let client: MongoClient
+ 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-
-  let globalWithMongoClientPromise = global as typeof globalThis & {
-    _mongoClientPromise: Promise<MongoClient>;
-  };
-
-  if (!globalWithMongoClientPromise._mongoClientPromise) {
-    client = new MongoClient(uri);
-    globalWithMongoClientPromise._mongoClientPromise = client.connect();
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient
   }
-
-  clientPromise = globalWithMongoClientPromise._mongoClientPromise;
+ 
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options)
+  }
+  client = globalWithMongo._mongoClient
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
+  client = new MongoClient(uri, options)
 }
-
-// Export a module-scoped MongoClient promise. By doing this in a
+ 
+// Export a module-scoped MongoClient. By doing this in a
 // separate module, the client can be shared across functions.
-export default clientPromise;
+export default client
